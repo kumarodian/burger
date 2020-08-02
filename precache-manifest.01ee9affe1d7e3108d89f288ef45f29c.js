00a689346bf090b3a3614a65dc5da3012b058536
kumarodian <sachinyadav607@gmail.com>
2020-08-02 12:02:17 +0530

    Updates

---
+++ precache-manifest.01ee9affe1d7e3108d89f288ef45f29c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f51d1f7a51987f84b41514dc9c6e87df",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "3051d45cedc125319633",
+    "url": "/burger/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "66ab2b783b2aaeb973d7",
+    "url": "/burger/static/js/2.133d4b1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/burger/static/js/2.133d4b1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3051d45cedc125319633",
+    "url": "/burger/static/js/main.9fd3995b.chunk.js"
+  },
+  {
+    "revision": "b70cca1f39cb2c1dd795",
+    "url": "/burger/static/js/runtime-main.95ddc443.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/burger/static/media/logo.5d5d9eef.svg"
+  }
+]);
